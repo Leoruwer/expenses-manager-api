d3e80a09c673e5a19b1711e6615d0bca3d2be74b
leoruwer <leonardo.ruwer@gmail.com>
2022-10-18 02:00:18 -0300

    Prettier :butterfly:

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, param:_username
+      resources :users
       post '/auth/login', to: 'authentication#login'
       get '/*a', to: 'application#not_found'
     end
