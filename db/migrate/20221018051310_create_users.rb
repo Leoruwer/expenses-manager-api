84251ccc06f63d2d5ebf86a238d4eff5ff77144d
leoruwer <leonardo.ruwer@gmail.com>
2022-10-18 02:26:55 -0300

    Create new migrations for user

---
+++ db/migrate/20221018051310_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name, nil: false
+      t.string :username, nil: false
+      t.string :email, nil: false
+      t.string :password_digest, nil: false
+      t.integer :role, default: 0
+
+      t.timestamps
+    end
+  end
+end
