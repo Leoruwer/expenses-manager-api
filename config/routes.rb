51ab7b247281643c9eda8a999b79025e256b6896
leoruwer <leonardo.ruwer@gmail.com>
2022-10-18 02:27:40 -0300

    Update routes to not include /api/v1 on request

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  namespace :api do
-    namespace :v1 do
-      resources :users
-      post '/auth/login', to: 'authentication#login'
-      get '/*a', to: 'application#not_found'
-    end
-  end
+  resources :users, param: :username
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
 end
