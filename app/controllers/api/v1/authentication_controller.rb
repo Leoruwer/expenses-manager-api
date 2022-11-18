57b68f68cc5b11bfdc4ea754d3e6034ae676f798
leoruwer <leonardo.ruwer@gmail.com>
2022-11-18 14:25:59 -0300

    Remove useless @

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,13 +2,13 @@ class AuthenticationController < ApplicationController
   before_action :authorize_request, except: :login
 
   def login
-    @user = User.find_by_email(params[:email])
+    user = User.find_by_email(params[:email])
 
-    if @user&.authenticate(params[:password])
-      token = JsonWebToken.encode(user_id: @user.id)
+    if user&.authenticate(params[:password])
+      token = JsonWebToken.encode(user_id: user.id)
       time = Time.now + 24.hours.to_i
       render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                     username: @user.username }, status: :ok
+                     username: user.username }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
