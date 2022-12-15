bc0f4b7cae34aa9e481522f21f680218307702fa
leoruwer <leonardo.ruwer@gmail.com>
2022-12-15 14:01:20 -0300

    Makes all models uses the slugify function

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
+  after_initialize :slugify_name
+
   primary_abstract_class
+
+  protected
+
+  def slugify_name
+    self.slug = "#{name}-#{SecureRandom.hex(3)}".downcase.parameterize if name.present?
+  end
 end
