3cddbd9cefd6d452d54bddd0bad5d6ca6ebe5532
leoruwer <leonardo.ruwer@gmail.com>
2022-12-15 14:03:09 -0300

    Prettier :butterfly:

--- spec/models/default_bill_spec.rb
+++ spec/models/default_bill_spec.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe DefaultBill, :type => :model do
+RSpec.describe DefaultBill do
   let!(:current_user) { create(:user) }
 
-  it "is valid with valid params" do
-    expect(DefaultBill.new(name: 'Default Bill', user: current_user)).to be_valid
+  it 'is valid with valid params' do
+    expect(described_class.new(name: 'Default Bill', user: current_user)).to be_valid
   end
 
-  it "generates slug" do
-    default_bill = DefaultBill.create(name: 'Default Bill', user: current_user)
+  it 'generates slug' do
+    default_bill = described_class.create(name: 'Default Bill', user: current_user)
 
     expect(default_bill.slug).not_to be_nil
   end
