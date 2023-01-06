59ed2aa2d14b91d69bf58be7e3b55b0f51efbe72
leoruwer <leonardo.ruwer@gmail.com>
2023-01-06 17:57:49 -0300

    Add faker to User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,11 @@
 
 FactoryBot.define do
   factory :user do
-    name { 'John Doe' }
-    slug { 'john-doe-1' }
-    email { 'john.doe@mail.com' }
-    password { 'securePassword123' }
+    user_name = Faker::Name.unique.name
+
+    name { user_name }
+    slug { "#{user_name}-#{SecureRandom.hex(3)}".downcase.parameterize }
+    email { Faker::Internet.email(name: user_name, separators: '.') }
+    password { Faker::Internet.password }
   end
 end
