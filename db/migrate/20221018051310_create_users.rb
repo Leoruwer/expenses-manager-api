e2354acb9386de81c076a88a6fe175b4bf855625
leoruwer <leonardo.ruwer@gmail.com>
2023-01-13 13:37:56 -0300

    Remove index for user slug

--- db/migrate/20221018051310_create_users.rb
+++ db/migrate/20221018051310_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name, null: false
       t.string :email, null: false, index: { unique: true }
-      t.string :slug, null: false, index: true
+      t.string :slug, null: false
       t.string :password_digest, null: false
       t.integer :role, default: 0
 
