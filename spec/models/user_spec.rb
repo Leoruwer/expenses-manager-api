37afe0b350c812abec8a992a235650af90a2ec4a
leoruwer <leonardo.ruwer@gmail.com>
2023-01-13 13:38:06 -0300

    Add User model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User do
+  let(:name) { 'User Name' }
+  let(:email) { 'user@mail.com' }
+  let(:password) { 'securePassword123' }
+
+  let(:params) do
+    {
+      name: name,
+      email: email,
+      password: password
+    }
+  end
+
+  context 'with valid params' do
+    it 'is valid' do
+      expect(described_class.new(params)).to be_valid
+    end
+
+    it 'generates slug' do
+      user = described_class.create(params)
+
+      expect(user.slug).to match('user-name')
+    end
+  end
+
+  describe 'model validations' do
+    context 'validates name presence' do
+      let(:name) { nil }
+
+      it 'is not valid' do
+        user = described_class.new(params)
+
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'validates email presence' do
+      let(:email) { nil }
+
+      it 'is not valid' do
+        user = described_class.new(params)
+
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'validates password presence' do
+      let(:password) { nil }
+
+      it 'is not valid' do
+        user = described_class.new(params)
+
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'validates email uniqueness' do
+      let!(:another_user) { create(:user, email: 'user@mail.com') }
+
+      it 'is not valid' do
+        user = described_class.new(params)
+
+        expect(user).not_to be_valid
+      end
+    end
+  end
+end
