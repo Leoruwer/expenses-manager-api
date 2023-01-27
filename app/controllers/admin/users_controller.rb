b840acfc34668da8dfaa30c9a5897791f86f3324
leoruwer <leonardo.ruwer@gmail.com>
2023-01-27 13:04:34 -0300

    Remove #create from admin/user_controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,6 @@
 module Admin
   class UsersController < ApplicationController
     before_action :authorize_only_admin
-    before_action :find_user, except: %i[create index]
 
     def index
       users = User.all
@@ -12,39 +11,39 @@ module Admin
     end
 
     def show
-      render json: @user, status: :ok
-    end
+      return render_user_not_found if user.blank?
 
-    def create
-      new_user = User.new(user_params)
-      if new_user.save
-        render json: new_user, status: :created
-      else
-        render json: { errors: new_user.errors.full_messages },
-               status: :unprocessable_entity
-      end
+      render json: user, status: :ok
     end
 
     def update
-      if @user.update(user_params)
+      return render_user_not_found if user.blank?
+
+      binding.pry
+
+      if user.update(user_params)
         render json: { message: 'User updated with success' }, status: :ok
       else
-        render json: { errors: @user.errors.full_messages },
+        render json: { errors: user.errors.full_messages },
                status: :unprocessable_entity
       end
     end
 
     def destroy
-      @user.destroy
+      return render_user_not_found if user.blank?
+
+      user.destroy
 
       render json: { message: 'User deleted with success' }, status: :ok
     end
 
     private
 
-    def find_user
-      @user = User.find_by!(slug: params[:slug])
-    rescue ActiveRecord::RecordNotFound
+    def user
+      @user ||= User.find_by(slug: params[:slug])
+    end
+
+    def render_user_not_found
       render json: { errors: 'User not found' }, status: :not_found
     end
 
