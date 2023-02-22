067bc406e37302edcd612db14675a39b4afbbdcb
leoruwer <leonardo.ruwer@gmail.com>
2023-02-22 14:45:27 -0300

    Create expenses models

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :default_bills, param: :slug
   resources :accounts, param: :slug
   resources :categories, param: :slug
+  resources :expenses, param: :slug
 
   post '/auth/login', to: 'authentication#login'
   post '/register', to: 'users#create'
